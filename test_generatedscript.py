d94f96c47b255b43717eb968dcb474e595d52eee
NikolG <greplova.nikol@gmail.com>
2026-03-02 19:28:16 +0100

    Locators

--- test_generatedscript.py
+++ test_generatedscript.py
@@ -3,13 +3,16 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://demo.playwright.dev/todomvc/#/")
-    page.get_by_role("textbox", name="What needs to be done?").click()
-    page.get_by_role("textbox", name="What needs to be done?").fill("Jokes")
-    page.get_by_role("textbox", name="What needs to be done?").press("Enter")
+    
+    todo_item = page.get_by_role("textbox", name="What needs to be done?")
+    todo_item.click()
+    todo_item.fill("Jokes")
+    todo_item.press("Enter")
+    
     page.get_by_role("checkbox", name="Toggle Todo").check()
     page.get_by_role("button", name="Clear completed").click()
 
